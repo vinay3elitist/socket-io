abe5fc2897ee2bceb9a6c14fc329c9658f400b17
vinay 3elitist <vinay.3elitist@gmail.com>
2024-06-12 19:30:10 +0530

    feat: DB Connection

--- chat-app/app.js
+++ chat-app/app.js
@@ -1,30 +1,83 @@
-const express = require('express');
-const { createServer } = require('http');
-const { join } = require('path');
-const { Server } = require('socket.io');
+const express = require("express");
+const { createServer } = require("http");
+const { join } = require("path");
+const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+const { availableParallelism } = require("os");
+const cluster = require("cluster");
+const { createAdapter, setupPrimary } = require("@socket.io/cluster-adapter");
+require("dotenv").config();
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {}
-});
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i,
+    });
+  }
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
+  return setupPrimary();
+}
 
-io.on('connection', (socket) => {
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+
+  const messageSchema = new mongoose.Schema({
+    clientOffset: { type: String, unique: true },
+    content: String,
+  });
+
+  const Message = mongoose.model("Message", messageSchema);
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    adapter: createAdapter(),
+  });
+
+  app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "index.html"));
+  });
+
+  io.on("connection", async (socket) => {
     console.log("Connection established");
+    socket.on("chat-message", async (chatMsg, clientOffset, callback) => {
+      let message;
+      try {
+        message = new Message({ content: chatMsg, clientOffset });
+        await message.save();
+      } catch (error) {
+        if (error.code === 11000) {
+          callback();
+        } else {
+          console.error(error);
+        }
+        return;
+      }
+      io.emit("chat-message", chatMsg, message._id);
+      callback();
+    });
 
-    socket.on('chat-message', (chatMsg) => {
-        io.emit('chat-message', chatMsg);
-    })
+    if (!socket.recovered) {
+      try {
+        const messages = await Message.find({
+          id: {
+            $gt: socket.handshake.auth.serverOffset || 0,
+          },
+        }).exec();
+        messages.forEach((message) => {
+          socket.emit("chat-message", message.content, message._id);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
 
-    socket.on('disconnect', () => {
-        console.log("Connection closed");
-    })
-})
+  server.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`);
+  });
+}
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+main();
